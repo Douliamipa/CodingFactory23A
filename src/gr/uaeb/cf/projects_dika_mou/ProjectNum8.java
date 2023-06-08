762d4e49a09cd9075894126a524c62b6140b1a78
Douliamipa <xrigiannakos@yahoo.com>
2023-06-08 07:32:14 +0300

    project4 v.2.0 working

--- src/gr/uaeb/cf/projects_dika_mou/ProjectNum8.java
+++ src/gr/uaeb/cf/projects_dika_mou/ProjectNum8.java
@@ -18,18 +18,43 @@ public class ProjectNum8 {
         }
 
         while (!(completeboard=isBoardComplete(board))){
-            roundAnouncer(getPreviusplayer(previusPlay));
-            if (previusPlay == 0 ){
-                players1play(board,getPreviusplayer(previusPlay));
-            } else if (previusPlay == 1) {
-                players2play(board , previusPlay);
-            }else {
-                players1play(board,previusPlay);
+            isBoardComplete(board);
+            printBoard(board);
+            for(int i = 2 ; i < 100 ; i++){
+                if (i % 2 == 0){
+                    System.out.println("Player's 1 turn");
+                    players1play(board);
+                    printBoard(board);
+                    areVictoryConditionsMet(board);
+                   if ( isBoardComplete(board) ){
+                       break;
+                   }
+
+                }else {
+                    System.out.println("Player's 2 turn");
+                    players2play(board);
+                    printBoard(board);
+                    areVictoryConditionsMet(board);
+                   if ( isBoardComplete(board)){
+                       break;
+                   }
+
+                }
             }
         }
+        System.out.println("The board is complete withought winners");
 
 
 
+    }
+    public static void printBoard(int[][] board){
+        for (int i = 0 ; i < board.length ; i++){
+            System.out.println();
+            for (int j = 0 ; j < board[i].length ; j++){
+                System.out.print(board[i][j] + " ");
+            }
+        }
+        System.out.println("");
     }
 
     public static  boolean isBoardComplete(int[][] board){
@@ -42,7 +67,7 @@ public class ProjectNum8 {
                 }
             }
         }
-        if (counter == (board.length * board[1].length)){
+        if (counter == (board.length * board[0].length)){
             completeBoard = true ;
         }
         return completeBoard ;
@@ -65,26 +90,27 @@ public class ProjectNum8 {
         }
     }
 
-    public static void  roundAnouncer(int previusPlay){
-        if ( previusPlay == 1){
-            System.out.println("Its player's numer 2 turn");
-        }else {
-            System.out.println("Its player's number 1 turn");
-        }
-    }
+//    public static void  roundAnouncer(int previusPlay){
+//        if ( previusPlay == 1){
+//            System.out.println("Its player's numer 2 turn");
+//        }else {
+//            System.out.println("Its player's number 1 turn");
+//        }
+//    }
 
-    public static void setPreviusPlay(int previusPlay){
-        if (previusPlay == 0){
-            previusPlay = 1 ;
-        }else if (previusPlay == 1){
-            previusPlay = 2 ;
-        }
-    }
-    public static int getPreviusplayer(int previusPlay){
-        return previusPlay;
-    }
+//    public static int setPreviusPlay(int previusPlay){
+//        if (previusPlay == 2){
+//            previusPlay = 1 ;
+//        }else if (previusPlay == 1){
+//            previusPlay = 2 ;
+//        }
+//        return previusPlay;
+//    }
+//    public static int getPreviusplayer(int previusPlay){
+//        return previusPlay;
+//    }
 
-    public static int[][] players1play (int[][] board , int previusPlay ){
+    public static int[][] players1play (int[][] board ) throws IllegalArgumentException{
         int choice = 0 ;
         boolean isPlayValid = false ;
         do {
@@ -99,7 +125,16 @@ public class ProjectNum8 {
             System.out.println("Press 8 if you want bottom midle position");
             System.out.println("Press 9 if you want bottom right corner");
 
-            choice = in.nextInt();
+            try {
+                choice = in.nextInt();
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+                System.out.println("you gave wrong input , try again");
+                choice = in.nextInt();
+                throw new IllegalArgumentException();
+
+
+            }
 
             switch (choice){
                 case 1 :
@@ -211,12 +246,12 @@ public class ProjectNum8 {
             }
 
 
-        }while (!isPlayValid);
-        setPreviusPlay(previusPlay);
+        }while (isPlayValid != true);
+        //setPreviusPlay(previusPlay);
 
         return board;
     }
-    public static int[][] players2play (int[][] board , int previusPlay ){
+    public static int[][] players2play (int[][] board  )throws IllegalArgumentException{
         int choice = 0 ;
         boolean isPlayValid = false ;
         do {
@@ -231,7 +266,16 @@ public class ProjectNum8 {
             System.out.println("Press 8 if you want bottom midle position");
             System.out.println("Press 9 if you want bottom right corner");
 
-            choice = in.nextInt();
+            try {
+                choice = in.nextInt();
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+                System.out.println("you gave wrong input , try again");
+                choice = in.nextInt();
+                throw new IllegalArgumentException();
+
+
+            }
 
             switch (choice){
                 case 1 :
@@ -344,7 +388,7 @@ public class ProjectNum8 {
 
 
         }while (!isPlayValid);
-        setPreviusPlay(previusPlay);
+       // setPreviusPlay(previusPlay);
 
         return board;
     }
